e393c3ede816070f91fc67697b0b81b04ccf9454
Ishmametov Ruslan <russ-i@yandex.ru>
2025-01-07 17:05:33 +0300

    inst mod

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let num = 5
-console.log(`Variable: ${num}`)
+const CC = require('currency-converter-lt')
+let currencyConverter = new CC({ from: "USD", to: "RUB", amount: 1 })
+
+currencyConverter.convert().then((response) => {
+    console.log(`Result: ${response}`) //or do something else
+})
+
