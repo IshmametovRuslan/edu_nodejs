47377e1fa09860c352e9489263a0026d5ae024e8
Ishmametov Ruslan <russ-i@yandex.ru>
2025-01-07 17:41:37 +0300

    _

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const CC = require('currency-converter-lt')
-let currencyConverter = new CC({ from: "USD", to: "RUB", amount: 1 })
+const os = require('os')
+let res = os.platform();
+console.log(`Platform: ${res}`)
 
-currencyConverter.convert().then((response) => {
-    console.log(`Result: ${response}`) //or do something else
-})
+
+const my_math = require(`./my_math`)
+let res1 = my_math.min(4, 10)
+let res2 = my_math.add(4, 10)
+console.log(`Res1 = ${res1}, Res2 = ${res2}`)
 
