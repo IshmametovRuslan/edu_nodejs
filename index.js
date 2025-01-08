15fd73be9f104ebfbf2beec4fb7a6c14c6eb9cd5
Ishmametov Ruslan <russ-i@yandex.ru>
2025-01-08 23:53:13 +0300

    express

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require('http')
 
-let server = http.createServer((req, res) => [
-
-])
+let server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    res.end('Hello <b>WORLD</b>')
+})
 
 const PORT = 3000
 const HOST = 'localhost'
