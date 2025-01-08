04aef17f02aabf74c99472c8e66fabf161ee5ccb
Ishmametov Ruslan <russ-i@yandex.ru>
2025-01-08 23:27:52 +0300

    serv

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const os = require('os')
-let res = os.platform();
-console.log(`Platform: ${res}`)
+const http = require('http')
 
+let server = http.createServer((req, res) => [
 
-const my_math = require(`./my_math`)
-let res1 = my_math.min(4, 10)
-let res2 = my_math.add(4, 10)
-console.log(`Res1 = ${res1}, Res2 = ${res2}`)
+])
 
+const PORT = 3000
+const HOST = 'localhost'
+
+server.listen(PORT, HOST, () => [
+    console.log(`Сервер запущен: http://${HOST}:${PORT}`)
+])
