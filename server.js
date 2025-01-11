a6b7fe9b30dd0498f92e292db0c0e81d1e409e3d
Ishmametov Ruslan <russ-i@yandex.ru>
2025-01-11 22:30:58 +0300

    express

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ import express from 'express'
 
 const app = express()
 
-
 async function main() {
-    
+    app.use(express.json())
+
+    app.use('/api/twit', (req, res) => {
+        res.json({
+            message: 'success'
+        }).status(200)
+    })
+
+    app.listen(4200, () => {
+        console.log('Server is runing is port 4200')
+    })
 }
+
+
+
+main()
