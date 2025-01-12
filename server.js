2a05e40451901f6df7180dc4802f4a3f26ad1ed2
Ishmametov Ruslan <russ-i@yandex.ru>
2025-01-12 16:25:29 +0300

    req

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from 'express'
+import { twitRouter } from './src/twit/twit.controller.js'
 
 const app = express()
 
 async function main() {
     app.use(express.json())
 
-    app.use('/api/twit', (req, res) => {
-        res.status(200).json({
-            message: 'success'
-        })
+    app.use('/api/twit', twitRouter)
+
+    app.all('*', (req, res) => {
+        res.status(404).json({ message: 'Not found'})
     })
 
     app.listen(3000, () => {
--- src/twit/twit.controller.js
+++ src/twit/twit.controller.js
@@ -1,13 +1,17 @@
-const { Router } = require("express");
-const { TwitService } = require("./twit.service");
+import { Router } from "express"
+import { TwitService } from "./twit.service.js"
 
-const router = Router
+const router = Router()
 
-const twitService = new TwitService()
+const twitSerwise = new TwitService
 
 router.post('/', (req, res) => {
-    const twit = twitService.createTwit(req, res)
+    if (!req.body?.text?.length) {
+        return res.status(400).json({ message: 'Message is required'})
+    }
+    const twit = twitSerwise.createTwit(req.body)
     res.status(201).json(twit)
 })
 
 export const twitRouter = router
+
